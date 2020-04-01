b669df7f1ef8bcc4bec2d6bcdb19209d7637c77c
ShantiLund <shanti.cs15@iba-suk.edu.pk>
2020-04-01 23:33:13 +0500

    create service which call restapi in order to upload the file

--- src/app/services/uploadVideo/upload-video.service.ts
+++ src/app/services/uploadVideo/upload-video.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { UrlSettings } from 'src/app/core/utilities/utils';
 import { map} from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class UploadVideoService {
   constructor(public http:HttpClient){}
    
   public upload(payload):Observable<any>{
-    return this.http.post(this.baseurl+'/upload/video', payload).pipe(map((res) => {
+     return this.http.post(this.baseurl+'upload/video', payload).pipe(map((res) => {
       console.log("Response success");
       return res;
     }, err => {
