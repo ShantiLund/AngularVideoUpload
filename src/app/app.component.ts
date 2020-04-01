367fd3929cabb06b4b2dfecdad7871e0ceca090f
ShantiLund <shanti.cs15@iba-suk.edu.pk>
2020-04-01 23:31:06 +0500

    video upload service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UploadVideoService } from 'src/app/services/uploadVideo/upload-video.service';
 import { Router } from '@angular/router';
+//import {HttpClient} from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   selectedFile:File=null;
- constructor(private uploadService:UploadVideoService){}
+  constructor(private uploadService:UploadVideoService){}
   onFileSelected(event){
     this.selectedFile=<File>event.target.files[0];
     console.log(event.target.files[0].name)  
@@ -20,8 +22,8 @@ export class AppComponent {
     const formData=new FormData();
     
     formData.append('video',this.selectedFile,this.selectedFile.name)
-    this.uploadService.upload(formData).subscribe(res=>{
-     console.log(res);
+     this.uploadService.upload(formData).subscribe(res=>{
+      console.log(res);
      }  );
 }
   }
